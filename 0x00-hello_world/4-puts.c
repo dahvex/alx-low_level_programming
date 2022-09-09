ace21460999c6edb5bc981be8e3b9d5da7311619
dahvex <davidedema17@gmail.com>
2022-09-08 17:28:05 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/**
+
+/*
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: always 0
  *
- *     */
+ * Return: always 0
+ *
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+			return (0);
 }
