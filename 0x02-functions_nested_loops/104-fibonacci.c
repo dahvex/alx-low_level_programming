f0fba4b9dd27f6c69ee1e74523f0a5e7194ba6fe
dahvex <davidedema17@gmail.com>
2022-09-13 20:03:40 -0700

    t 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
- *     1 and 2, separated by a comma followed by a space.
+ * 1 and 2, separated by a comma followed by a space.
  *
- *  Return: Always 0.
+ * Return: Always 0.
  *
  */
 int main(void)
@@ -18,7 +18,6 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-
 
 		fib1 = fib2;
 		fib2 = sum;
