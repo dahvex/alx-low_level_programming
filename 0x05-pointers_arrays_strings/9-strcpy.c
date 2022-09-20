547cafafb4174cb28e687924b7110be2d0c13b04
dahvex <davidedema17@gmail.com>
2022-09-19 20:47:33 -0700

    tasks

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i <= _strlen(src); i++;
+	for (i = 0; i <= _strlen(src); i++)
 	{
-	dest[i] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
